eaa5a7ef6790fad0be387189499023dd0d54e8ab
uguisu-an <arcana278@gmail.com>
2020-01-02 05:46:41 +0900

    lintの設定を調整した

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint"
   ],
   parserOptions: {
-    project: './tsconfig.json',
+    project: "./tsconfig.json",
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
-  rules: {
-    // Special ESLint rules or overrides go here.
-  },
-}
+  rules: {}
+};
