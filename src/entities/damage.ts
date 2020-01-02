d7651b7138231b4007e1c6917933e7469b8d95f9
uguisu-an <arcana278@gmail.com>
2020-01-02 14:04:59 +0900

    ダメージへの補正処理を変更した

--- src/entities/damage.ts
+++ src/entities/damage.ts
@@ -1,6 +1,26 @@
-export class BaseDamage {
+/**
+ * ダメージ
+ */
+export class Damage {
   public readonly damage: number;
 
+  public constructor(damage: number) {
+    this.damage = damage;
+  }
+
+  /**
+   * 補正をかける
+   * @param scale 補正値
+   */
+  public scale(scale: number): Damage {
+    return new Damage(Math.floor(this.damage * scale));
+  }
+}
+
+/**
+ * 補正なしのダメージ
+ */
+export class BaseDamage extends Damage {
   public constructor(
     atk: number,
     def: number,
@@ -8,6 +28,7 @@ export class BaseDamage {
     level: number = 50
   ) {
     const lv = Math.floor((level * 2) / 5 + 2);
-    this.damage = Math.floor(Math.floor((lv * power * atk) / def) / 50 + 2);
+    const damage = Math.floor(Math.floor((lv * power * atk) / def) / 50 + 2);
+    super(damage);
   }
 }
--- test/entities/damage.test.ts
+++ test/entities/damage.test.ts
@@ -1,8 +1,13 @@
 import { BaseDamage } from "../../src/entities/damage";
 
-describe("BaseDamage", () => {
-  it("とくこう130族からとくぼう130族へのはかいこうせん", () => {
+describe("Damage", () => {
+  describe("とくこう130族からとくぼう130族へのはかいこうせん", () => {
     const damage = new BaseDamage(200, 200, 150);
-    expect(damage.damage).toBe(68);
+    it("最大乱数（補正なし）", () => {
+      expect(damage.damage).toBe(68);
+    });
+    it("最小乱数", () => {
+      expect(damage.scale(0.85).damage).toBe(57);
+    });
   });
 });
