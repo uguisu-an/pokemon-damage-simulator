83c49190cccfb198f00ab27d4676291cb35f8942
uguisu-an <arcana278@gmail.com>
2020-01-02 15:23:14 +0900

    typeの設定を簡略化した

--- src/entities/type.ts
+++ src/entities/type.ts
@@ -1,113 +1,3 @@
-export default class Type {
-  private name: TypeName;
-  private effect: TypeMap;
-
-  public constructor(name: TypeName, effect: TypeMap) {
-    this.name = name;
-    this.effect = effect;
-  }
-
-  public equals(type: Type): boolean {
-    return this.name === type.name;
-  }
-
-  public affect(type: Type): number {
-    return this.effect[type.name];
-  }
-
-  public static normal(): Type {
-    return new Type("normal", {
-      normal: 1,
-      fire: 1,
-      water: 1,
-      grass: 1,
-      electric: 1,
-      ice: 1,
-      fighting: 1,
-      poison: 1,
-      ground: 1,
-      flying: 1,
-      psychic: 1,
-      bug: 1,
-      rock: 0.5,
-      ghost: 0,
-      dragon: 1,
-      dark: 1,
-      steel: 0.5,
-      fairy: 1
-    });
-  }
-
-  public static rock(): Type {
-    return new Type("rock", {
-      normal: 1,
-      fire: 1,
-      water: 1,
-      grass: 1,
-      electric: 1,
-      ice: 1,
-      fighting: 1,
-      poison: 1,
-      ground: 1,
-      flying: 1,
-      psychic: 1,
-      bug: 1,
-      rock: 1,
-      ghost: 1,
-      dragon: 1,
-      dark: 1,
-      steel: 1,
-      fairy: 1
-    });
-  }
-
-  public static steel(): Type {
-    return new Type("steel", {
-      normal: 1,
-      fire: 1,
-      water: 1,
-      grass: 1,
-      electric: 1,
-      ice: 1,
-      fighting: 1,
-      poison: 1,
-      ground: 1,
-      flying: 1,
-      psychic: 1,
-      bug: 1,
-      rock: 1,
-      ghost: 1,
-      dragon: 1,
-      dark: 1,
-      steel: 1,
-      fairy: 1
-    });
-  }
-
-  public static ghost(): Type {
-    return new Type("ghost", {
-      normal: 1,
-      fire: 1,
-      water: 1,
-      grass: 1,
-      electric: 1,
-      ice: 1,
-      fighting: 1,
-      poison: 1,
-      ground: 1,
-      flying: 1,
-      psychic: 1,
-      bug: 1,
-      rock: 1,
-      ghost: 1,
-      dragon: 1,
-      dark: 1,
-      steel: 1,
-      fairy: 1
-    });
-  }
-}
-
 type TypeName =
   | "normal"
   | "fire"
@@ -147,4 +37,69 @@ interface TypeMap {
   dark: number;
   steel: number;
   fairy: number;
+}
+
+function types(types: Partial<TypeMap>): TypeMap {
+  return {
+    normal: 1,
+    fire: 1,
+    water: 1,
+    grass: 1,
+    electric: 1,
+    ice: 1,
+    fighting: 1,
+    poison: 1,
+    ground: 1,
+    flying: 1,
+    psychic: 1,
+    bug: 1,
+    rock: 1,
+    ghost: 1,
+    dragon: 1,
+    dark: 1,
+    steel: 1,
+    fairy: 1,
+    ...types
+  };
+}
+
+export default class Type {
+  private name: TypeName;
+  private effect: TypeMap;
+
+  public constructor(name: TypeName, effect: TypeMap) {
+    this.name = name;
+    this.effect = effect;
+  }
+
+  public equals(type: Type): boolean {
+    return this.name === type.name;
+  }
+
+  public affect(type: Type): number {
+    return this.effect[type.name];
+  }
+
+  public static normal(): Type {
+    return new Type(
+      "normal",
+      types({
+        rock: 0.5,
+        ghost: 0,
+        steel: 0.5
+      })
+    );
+  }
+
+  public static rock(): Type {
+    return new Type("rock", types({}));
+  }
+
+  public static steel(): Type {
+    return new Type("steel", types({}));
+  }
+
+  public static ghost(): Type {
+    return new Type("ghost", types({}));
+  }
 }
